f39b5f64368b4e70ff59d99d8f3f5a6feb51f443
Javier Zapata <zapatajavier34@gmail.com>
2024-01-24 12:17:20 +1100

    Ready the app of cats, now we gonna put custom hooks

--- 05-technic-trial/src/app.jsx
+++ 05-technic-trial/src/app.jsx
@@ -1,9 +1,43 @@
+import { useEffect, useState } from "react"
+import './app.css'
+
 export function App () {
+    
+    const [fact, setFact] = useState()
+    const [image, setImageUrl] = useState()
+    const [error, setError] = useState()
+    
+    const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+    /* const CAT_ENDPOINT_IMAGE_URL = `` */
+
+    useEffect(() => {
+        fetch(CAT_ENDPOINT_RANDOM_FACT)
+        .then(res => {
+            /* Manejar errores de ultimo y resolver primero el happy Path, luego si se continua con las buenas practicas */
+            if(!res.ok) {
+                setError('No se pudo recuperar la cita')
+            }
+            return res.json()
+        })
+        .then(data => {
+            const { fact } = data
+            setFact(fact)
+        })
+    }, [])
+
+    useEffect(() => {
+        if(!fact) return
+            /* const firstWord = fact.split(' ').slice(0, 3).join(' ') */
+            const threeWords = (fact.split(' ', 3).join(' '))
+            console.log(threeWords)
+            setImageUrl(`https://cataas.com/cat/says/${threeWords}?size=50&color=red`)
+    }, [fact])
+
     return(
-        <div>
-            <main>
-                square
-            </main>
-        </div>
+        <main className="appDeGatitos">
+            <h1>APP DE GATITOS</h1>
+            {fact && <span>{fact}</span>}
+            {image && <img src={image} alt={`Image extracted using the first three words from ${fact}`}/ >}
+        </main>
     )
 }
